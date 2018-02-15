bbc8dafaf21a563d552d4b111da6c6235b400d22
ItClown <vallier.jerem@gmail.com>
2018-02-15 16:28:07 +0100

    User

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/key');
 //const mongoose = require('mongoose');
-const User = mongoose.model('users');
+//const User = mongoose.model('users');
 
 
 passport.serializeUser((user, done) => {
