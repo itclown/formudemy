35243588bfe9e4add82bffe1ad1b71c765d32714
ItClown <vallier.jerem@gmail.com>
2018-02-15 15:20:42 +0100

    User

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/key');
 
+const User = require('../models/User');
+
 
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -12,6 +14,15 @@ passport.use(new GoogleStrategy({
         console.log('Access token', accessToken);
         console.log('Refresh token', refresToken);
         console.log('Profile', profile);
+
+        User.findOne({ googleId: profile.id }).then(existingUser => {
+            if(existingUser) {
+                done(null, existingUser);
+            }
+            else {
+                new User({ googleId: profile.id }).save().then(user => done(null, user));
+            }
+        });
     }
     )
 );
