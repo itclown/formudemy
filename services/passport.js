2c9389482d00bc7b6712013df9847dd3dbe4a545
ItClown <vallier.jerem@gmail.com>
2018-02-15 15:28:55 +0100

    User

--- services/passport.js
+++ services/passport.js
@@ -8,8 +8,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
+        callbackURL: '/auth/google/callback'
     }, (accessToken, refresToken, profile, done) => {
         console.log('Access token', accessToken);
         console.log('Refresh token', refresToken);
